2a74c30f47644befb88fbdcabaaa795bb50bb45d
Gilad Ben-Avi <gilad.benavi@appsflyer.com>
2023-10-11 13:50:56 +0300

    Add Java

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,6 +6,7 @@ return {
 
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.java" },
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
